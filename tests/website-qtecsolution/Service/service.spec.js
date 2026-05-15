fbd65e92add251f0d7694778df30054718b2d1a0
rashed <rashed987654@gmail.com>
2026-05-15 07:32:50 +0600

    Update service.spec.js

--- tests/website-qtecsolution/Service/service.spec.js
+++ tests/website-qtecsolution/Service/service.spec.js
@@ -8,7 +8,7 @@ test.describe("Services page", () => {
     await page.goto(BASE_URL);
 });
 
-//This is Test 5
+
 
 // 1️⃣ Header: 
 test("Header 👉 1a. Verify Services Menu & Submenu (Visibility, Hover, Click & URL)", async ({ page }) => {
